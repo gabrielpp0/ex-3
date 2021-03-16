ee89e404bbe767d84d048f44562c0aace3533afd
gabrielpp0 <71361726+gabrielpp0@users.noreply.github.com>
2021-03-16 12:03:14 -0400

    final

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -4,13 +4,7 @@ import 'firebase/firestore';
 /******* Ex#3 - Étape B ********************************/ 
 // Ajouter votre objet de configuration Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBleqrll-h-CdCvOxW4CIn6QOF1deUMb4A",
-  authDomain: "ex-3-37a91.firebaseapp.com",
-  projectId: "ex-3-37a91",
-  storageBucket: "ex-3-37a91.appspot.com",
-  messagingSenderId: "75050987897",
-  appId: "1:75050987897:web:6d33e4b62dc4f34cdb7fa8",
-  measurementId: "G-QWZX7DKV1Z"
+
 };
 
 // Initialiser Firebase
